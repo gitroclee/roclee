ab02f0b6d00475ce764d4605fdc9f59c62350c9c
roclee <roc_vip@126.com>
2023-07-07 10:06:26 +0800

    1·1

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 create table test_python_sql
 (
-username varchar2(20),
-passwd varchar2(20)
+username varchar(20),
+passwd varchar(20)
 );
 
 insert into test_python_sql values ('roclee','shagua1314');
